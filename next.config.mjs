171aadd1953d2e77bdf5b891aabcec6eadff71b3
buddhamac1 <702buddha@gmail.com>
2026-03-17 23:05:08 -0700

    Add TypeScript and ESLint configuration options

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
-  };
+    typescript: {
+          ignoreBuildErrors: true,
+    },
+    eslint: {
+          ignoreDuringBuilds: true,
+    },
+};
 
-  export default nextConfig;
+export default nextConfig;
